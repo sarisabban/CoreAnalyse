66f3b92eb857cbaba355b645ada7bfadfa1ed0a2
Sari Sabban <sari.sabban@gmail.com>
2017-02-18 19:33:09 +0300

    Update CoreAnalyse.py

--- CoreAnalyse.py
+++ CoreAnalyse.py
@@ -20,7 +20,13 @@ structure=p.get_structure('X',filename)		# |Standard structure to setup biopytho
 model=structure[0]				# |
 dssp=DSSP(model,filename,acc_array='Wilke')	#_|
 
-print('Found polar residues in core:')
+
+for x in sys.argv:
+	if x=='-v':
+		print('Found polar residues in core:')
+	else:
+		continue
+
 reslist=list()
 count=0
 for x in dssp:					#Loop to isolate SASA for each amino acid
@@ -47,17 +53,32 @@ for x in dssp:					#Loop to isolate SASA for each amino acid
 
 #Calculate if polar amino acids (DEKHRQNSTY) are in core and print them
 	if (x[2]=='G' or x[2]=='H' or x[2]=='I' or x[2]=='B' or x[2]=='E') and (x[1]=='D' or x[1]=='E' or x[1]=='K' or x[1]=='H' or x[1]=='R' or x[1]=='Q' or x[1]=='N' or x[1]=='S' or x[1]=='T' or x[1]=='Y') and sasa <= 15:		#In helix or sheet
-		print(x[0],x[1])
+		for verb in sys.argv:
+			if verb=='-v':
+				print(x[0],x[1])
+			else:
+				continue
+
 		count=count+1
 		reslist.append(x[0])
 	if (x[2]=='-' or x[2]=='T' or x[2]=='S') and (x[1]=='D' or x[1]=='E' or x[1]=='K' or x[1]=='H' or x[1]=='R' or x[1]=='Q' or x[1]=='N' or x[1]=='S' or x[1]=='T' or x[1]=='Y') and sasa <= 25:					#In loop
-		print(x[0],x[1])
+		for verb in sys.argv:
+			if verb=='-v':
+				print(x[0],x[1])
+			else:
+				continue
+
 		count=count+1
 		reslist.append(x[0])
 
 #Print results
-print('-----------------')
-print('Pymol command:')
-print('select PolarCoreRes, resi','+'.join(str(x) for x in reslist))
-print('-----------------')
-print('Polar residues in core:',count)
+for x in sys.argv:
+	if x=='-v':
+		print('-----------------')
+		print('Pymol command:')
+		print('select PolarCoreRes, resi','+'.join(str(x) for x in reslist))
+		print('-----------------')
+	else:
+		continue
+
+print(sys.argv[1],'\t','Number of polar residues in core:','\t',count)
